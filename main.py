fda21b2a0bee1712d5d86abbd4c71ebe04c90dd2
Yunehs <kozylov03@mail.ru>
2023-04-10 01:33:56 +0300

    Добавили изменённый файл main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import random
 
 def password():
     s1 = ['a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's']
+    s2 = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S']
     result = ''
     for i in range(11):
-        result += random.choice(s1)
+        result += random.choice(s1+s2)
     return result
     
 p = password()
